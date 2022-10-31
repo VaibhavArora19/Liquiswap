688a6b84aa22f69ca1d43e73f27fa95b4bafe092
phill <56482955+6str@users.noreply.github.com>
2022-10-31 12:15:14 +0000

    Update LiquiUniSwap.sol

--- contracts/LiquiUniSwap.sol
+++ contracts/LiquiUniSwap.sol
@@ -18,76 +18,73 @@ interface IERC20{
         uint256 amount
     ) external returns (bool);
     function approve(address spender, uint256 amount) external returns (bool);
-
 }
 
+contract LiquiSwap is AutomationCompatibleInterface {
 
-// ---> added  'is AutomationCompatibleInterface'
-contract SwapExamples is AutomationCompatibleInterface {
-    // For the scope of these swap examples,
-    // we will detail the design considerations when using
-    // `exactInput`, `exactInputSingle`, `exactOutput`, and  `exactOutputSingle`.
+    event UserAdded(address indexed user);
+    event UserDeleted(address indexed user);
+    event Deposit(address indexed user, string token, uint amount);
+    event Withdrawal(address indexed user, string token, uint amount);
+    event Liquidation(int indexed price, uint targetAmount, uint actualAmount);
 
-    // It should be noted that for the sake of these examples, we purposefully pass in the swap router instead of inherit the swap router for simplicity.
-    // More advanced example contracts will detail how to inherit the swap router safely.
+    address public ETHvUSD = 0x0715A7794a1dc8e42615F059dD6e406A6594651A;
+    AggregatorV3Interface internal priceFeed = AggregatorV3Interface(ETHvUSD);
+
     address public constant routerAddress = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
-    ISwapRouter public immutable swapRouter = ISwapRouter(routerAddress);
+    ISwapRouter internal immutable swapRouter = ISwapRouter(routerAddress);
 
-    // This example swaps DAI/WETH9 for single path swaps and DAI/USDC/WETH9 for multi path swaps.
+    address public constant WETH9 = 0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa;
+    IERC20 internal WETHToken = IERC20(WETH9);
 
     address public constant DAI = 0x001B3B4d0F3714Ca98ba10F6042DaEbF0B1B7b6F;
-    address public constant WETH9 = 0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa;
-    address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-    IERC20 public WETHToken = IERC20(WETH9);
+    IERC20 internal DAIToken = IERC20(DAI);
 
     // For this example, we will set the pool fee to 0.3%.
     uint24 public constant poolFee = 3000;
 
-
-// ---> added state variables
     address owner;
 
-    struct user {
-        uint usersIndexPosition;
-        int liquidationPrice;
-    }
+    uint contractBalanceWETH;
+    uint contractBalanceDAI;
 
     /// @dev keeps track of users and their liquidation/stop prices
     /// mapping for lookups, array for iterating
     address[] public usersIndex;
     mapping(address => user) public users;
 
-    AggregatorV3Interface internal priceFeed;
+    struct user {
+        uint usersIndexPosition;
+        int liquidationPrice;
+        uint balanceWETH;
+        uint balanceDAI;
+    }
 
-    // for testing only
+
+// for testing only
+    uint public runCountPerformUpkeep;
+    uint public upkeepRanCount;
     bool public upkeepNeeded_;
     bytes public upkeepData_;
-    int testingPrice = 139000000000;
+    // int testingPrice = 139000000000;
 
-    address[] public testingUsersLiquidated;
-    int[] public testingPricesLiquidated;
+    // address[] public testingUsersLiquidated;
+    // int[] public testingPricesLiquidated;
     /// end  // for testing only
     // performUpkeep test performData
     // 0x4b0897b0513fdc7c541b6d9d7e929c4e5364d2db14723a09acff6d2a60dcdf7aa4aff308fddc160cca35b7d915458ef540ade6068dfe2f44e8fa733c03c6fced478cbbc9a4fab34ef9f40767739d1ff7
-// <--- 
+// testing
 
-// ---> added construct and modifier
     constructor () {
-        priceFeed = AggregatorV3Interface(0x0715A7794a1dc8e42615F059dD6e406A6594651A);
         owner = msg.sender;
-        // add dummy first user so a userIndexPosition of 0 can be assumed to mean user doesn't exist in mapping and corresponding array
-        addUser(address(0), 0);
-        
-        // for testing only
-        //setupTestData();
+        // add dummy first user so it's certain a userIndexPosition test returning 0 means user doesn't exist in mapping and corresponding array
+        usersIndex.push(address(0));
     }
 
     modifier onlyOwner {
         require(msg.sender == owner, "not owner");
         _;
     }
-// <---
-
 
 // ---> get latest price for ETH/USD - this can be replaced by something else
     /**
@@ -108,35 +105,93 @@ contract SwapExamples is AutomationCompatibleInterface {
 // ---> added add / delete user functions
     // @ todo: secure who can execute function
     //  : public while testing
-    function addUser(address _addr, int _liquidationPrice) public {
-        require(users[_addr].usersIndexPosition == 0, 'user already added');
-        usersIndex.push(_addr);
-        users[_addr] = user({usersIndexPosition: usersIndex.length, liquidationPrice: _liquidationPrice});
+
+    function addUser() public {
+        require(users[msg.sender].usersIndexPosition == 0, 'user already added');
+
+        uint amountIn = WETHToken.allowance(msg.sender, address(this));
+        WETHToken.transferFrom(msg.sender, address(this), amountIn);
+        contractBalanceWETH += amountIn;
+        usersIndex.push(msg.sender);
+        users[msg.sender] = user({usersIndexPosition: usersIndex.length, liquidationPrice: -1, balanceWETH: amountIn, balanceDAI: 0});
+        emit UserAdded(msg.sender);
+    }
+
+    
+    /// @dev change the liquidation price for an existing user
+    // @ todo: secure who can execute function
+    function setLiquidationPrice(int _liquidationPrice) external onlyOwner {
+        require(users[msg.sender].usersIndexPosition != 0, 'not a user');
+        require(users[msg.sender].balanceWETH >= 0, 'no balance');
+        users[msg.sender].liquidationPrice = _liquidationPrice;
     }
 
 
     // @ todo: secure who can execute function
     //  : public while testing
-    function delUser(address _addr) public {
-        require(users[_addr].usersIndexPosition != 0, 'not a user');
-        uint _pos = users[_addr].usersIndexPosition;
+    function delUser() public {
+        require(users[msg.sender].usersIndexPosition != 0, 'not a user');
+        require(users[msg.sender].balanceWETH == 0, 'WETH balance > 0');
+        require(users[msg.sender].balanceDAI == 0, 'DAI balance > 0');
+        uint _pos = users[msg.sender].usersIndexPosition;
         usersIndex[_pos] = usersIndex[usersIndex.length -1];
         usersIndex.pop();
-        delete users[_addr];
+        delete users[msg.sender];
+        emit UserDeleted(msg.sender);
     }
 
-    /// @dev change the liquidation price for an existing user
-    // @ todo: secure who can execute function
-    function changeLiquidationPrice(address _addr, int _liquidationPrice) external onlyOwner {
-        require(users[_addr].usersIndexPosition != 0, 'not a user');
-        users[_addr].liquidationPrice = _liquidationPrice;
-    }
-// <---
 
-// ---> added chainlink automation upkeep functions
-    function checkUpkeep(bytes calldata checkData) external override returns (bool upkeepNeeded, bytes memory performData) {
+    function getBalanceWETH() public view returns (uint) {
+        return users[msg.sender].balanceWETH;
+    }
+
+
+    function depositWETH(uint _amount) external {
+        if(users[msg.sender].usersIndexPosition == 0) addUser();
+        require(_amount <= WETHToken.allowance(msg.sender, address(this)), 'amount > approval');
+
+        WETHToken.transferFrom(msg.sender, address(this), _amount);
+        contractBalanceWETH += _amount;
+        users[msg.sender].balanceWETH += _amount;
+        emit Deposit(msg.sender, "WETH", _amount);
+    }
+
+
+    function withdrawWETH(uint _amount) external {
+        uint _balance = getBalanceWETH();
+        require(_amount <= _balance, 'amount > balance');
+        WETHToken.transfer(msg.sender, _amount);
+        users[msg.sender].balanceWETH -=_amount;
+        emit Withdrawal(msg.sender, "WETH", _amount);
+    }
+
+
+    function getBalanceDAI() public view returns (uint) {
+        return users[msg.sender].balanceDAI;
+    }
+
+
+    function withdrawDAI(uint _amount) external {
+        uint _balance = getBalanceDAI();
+        require(_amount <= _balance, 'amount > balance');
+        DAIToken.transfer(msg.sender, _amount);
+        users[msg.sender].balanceWETH -=_amount;
+        emit Withdrawal(msg.sender, "DAI", _amount);
+    }
+
+
+    function getContractBalanceWETH() public view returns(uint256){
+        return WETHToken.balanceOf(address(this));
+    }
+
+
+    function getContractBalanceDAI() public view returns(uint256){
+        return DAIToken.balanceOf(address(this));
+    }
+
+
+    function checkUpkeep(bytes calldata checkData) external view override returns (bool upkeepNeeded, bytes memory performData) {
         int _price = getLatestPrice();
-        //int _price = getLatestPriceTesting(); // for testing only
 
         for(uint i = 1; i < usersIndex.length; ++i) {
             if(_price <= users[usersIndex[i]].liquidationPrice) {
@@ -144,64 +199,69 @@ contract SwapExamples is AutomationCompatibleInterface {
             }
         }
         if(performData.length > 0) upkeepNeeded = true;
-        
-        //  for testing only
-        upkeepNeeded_ = upkeepNeeded;
-        upkeepData_ = performData;
     }
 
 
+    /// @dev each step rechecks the trigger condition as function can be called 
     function performUpkeep(bytes calldata performData) external override {
         
         int _price = getLatestPrice();
-        //int _price = getLatestPriceTesting(); // for testing only
 
+        uint liquidationAmount;
         address _user;
+
+        // re-check of condition required
         for(uint _startPos; _startPos < performData.length; _startPos += 20) {
             _user = address(bytes20(performData[_startPos:_startPos + 20]));
 
             if(_price <= users[_user].liquidationPrice) {
-                liquidateUser(_user);
+                liquidationAmount += users[_user].balanceWETH;
             }
         }
-    }
-// <---
 
-// ---> added liquidate : 
+        if(liquidationAmount > 0) {
+
+            uint amountLiquidated = Liquidate(liquidationAmount);
+            
+            contractBalanceWETH -= amountLiquidated;
+            contractBalanceDAI += amountLiquidated;
+
+            // update the user account balances
+            if(amountLiquidated == liquidationAmount) {
+                for(uint _startPos; _startPos < performData.length; _startPos += 20) {
+                    if(_price <= users[_user].liquidationPrice) {
+                        _user = address(bytes20(performData[_startPos:_startPos + 20]));
+                        users[_user].balanceDAI += users[_user].balanceWETH;
+                        users[_user].balanceWETH = 0;
+                        users[_user].liquidationPrice = -1;
+                    }
+                }
+            } else {    // partial liquidation. apportion shares of the liquidation
+                for(uint _startPos; _startPos < performData.length; _startPos += 20) {
+                    _user = address(bytes20(performData[_startPos:_startPos + 20]));
+
+                    if(_price <= users[_user].liquidationPrice) {
+                        _user = address(bytes20(performData[_startPos:_startPos + 20]));
+                        uint shareOfLiquidation = (amountLiquidated * users[_user].balanceWETH) / liquidationAmount;
+                        users[_user].balanceDAI += shareOfLiquidation;
+                        users[_user].balanceWETH -= shareOfLiquidation;
+                    }
+                }
+            }
+            emit Liquidation(_price, liquidationAmount, amountLiquidated);
+        }
+    }
+
     
-//  *** code to do the swap needs to be added ***
-    
-    /// @dev liquidate the user's position
-    function liquidateUser(address _user) internal {
-        // think about : if there's a problem liquidating 1 user's tokens, does that mean all liquidations fail/revert or can other liquidation succeed?
-        // todo: ensure the loop can't exceed block gas limit
-/*
-    -->
-    ---->  *** liquidation code goes here ***
-    -->
-*/
-        // all tokens liquidated. won't trigger again unless changed. perhaps better to achieve this by detecting the user's balance
-        users[_user].liquidationPrice = -1; 
-    }
-// <---
-
-
-
-    function transferToContract(uint256 amountIn) external {
-        // msg.sender must approve this contract
-
-        // Transfer the specified amount of DAI to this contract.
-        WETHToken.transferFrom(msg.sender, address(this), amountIn);
-    }
-
     /// @notice swapExactInputSingle swaps a fixed amount of DAI for a maximum possible amount of WETH9
     /// using the DAI/WETH9 0.3% pool by calling `exactInputSingle` in the swap router.
     /// @dev The calling address must approve this contract to spend at least `amountIn` worth of its DAI for this function to succeed.
     /// @param amountIn The exact amount of DAI that will be swapped for WETH9.
     /// @return amountOut The amount of WETH9 received.
-    function swapExactInputSingle(uint256 amountIn) external returns (uint256 amountOut) {
+    function Liquidate(uint256 amountIn) public returns (uint256 amountOut) {
+// todo: secure so can only be run internally
 
-        // Approve the router to spend DAI.
+        // Approve the router to spend WETH.
         WETHToken.approve(address(swapRouter), amountIn);
 
         // Naively set amountOutMinimum to 0. In production, use an oracle or other data source to choose a safer value for amountOutMinimum.
@@ -211,7 +271,7 @@ contract SwapExamples is AutomationCompatibleInterface {
                 tokenIn: WETH9,
                 tokenOut: DAI,
                 fee: poolFee,
-                recipient: msg.sender,
+                recipient: address(this),
                 deadline: block.timestamp,
                 amountIn: amountIn,
                 amountOutMinimum: 0,
@@ -224,18 +284,29 @@ contract SwapExamples is AutomationCompatibleInterface {
         return amountOut;
     }
 
-        
-        /// @dev - for testing - get token back out of the contract
-        function send() external{
-            uint256 amt = WETHToken.balanceOf(address(this));
-            WETHToken.transfer(msg.sender, amt);
-        }
 
-        function getBalance() public view returns(uint256){
-            return WETHToken.balanceOf(address(this));
-        }
 
-        function checkAllowance(address owner, address spender) external view returns(uint256){
-            return WETHToken.allowance(owner, spender);
-        }
+    // now using functiondepositWETH
+    // function transferToContract(uint256 amountIn) external {
+    //     // msg.sender must approve this contract
+
+    //     // Transfer the specified amount of DAI to this contract.
+    //     WETHToken.transferFrom(msg.sender, address(this), amountIn);
+    // }
+
+    // use withdrawWeth / withdrawDAI
+    /// @dev - for testing - get all tokens back out of the contract
+    // function send() external{
+    //     uint256 amt = WETHToken.balanceOf(address(this));
+    //     WETHToken.transfer(msg.sender, amt);
+    // }
+
+    // // if needed can be done on the token contract
+    // function checkAllowance(address owner, address spender) external view returns(uint256){
+    //     return WETHToken.allowance(owner, spender);
+    // }
+
+    function getAllowance() external view returns (uint) {
+            return WETHToken.allowance(msg.sender, address(this));
+    }
 }
