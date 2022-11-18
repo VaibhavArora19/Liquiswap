46729d486432a83f6fdd04f11309247d74774b23
Trymbak Mahant <trymbak0102mahant@gmail.com>
2022-11-18 21:05:26 +0530

    server update

---
+++ server/db.js
@@ -0,0 +1,16 @@
+//const mongoose = require("mongoose");
+import mongoose from "mongoose";
+export const connection = () => {
+  const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+  try {
+    mongoose.connect(process.env.DB, connectionParams);
+
+    console.log("Connected to database successfully");
+  } catch (error) {
+    console.log(error);
+    console.log("Could not connect database!");
+  }
+};
